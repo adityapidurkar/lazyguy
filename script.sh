545cad07a0e6988dc88f7ec1197b2d6a5a5baf3f
T-Hacker <78270259+T-Hackers@users.noreply.github.com>
2022-01-10 12:29:27 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,173 @@
+#script By T-Hacker
+#This Script Is Made For The Users Of
+# HackersSpace Server
+#Join The Server Here
+#https://discord.gg/9429UqrujD
+#Subscribe My YouTube Channel To Learn Termux.
+# https://youtube.com/channel/UCqo9MRF6RNv210VYN4a48tg
+#The Tools Starts From Here
+
+clear
+echo Loading ██ 20%
+sleep 0.5
+clear
+echo loading ███ 40%
+sleep 0.5
+clear
+echo loading ████ 60%
+sleep 0.5
+clear
+echo loading █████ 80%
+sleep 0.5
+clear
+echo loading ██████ 100%
+sleep 0.5
+clear
+echo Loaded Successfully.
+clear
+cd $home
+clear
+echo Made By:- T-Hacker
+echo Join Our Discord Server
+echo
+echo
+echo "Press Y when the process asks for y/n"
+echo "and press enter when the process asks for N/default"
+echo "The Tool Will Take Approximately 20 min to install all the things."
+
+echo "The Tool has 3 stages:-"
+echo "1.Installing Packages"
+echo "2.Installing Python Modules"
+echo "3.Creating Folders"
+echo "4.Cloning Repositories"
+
+sleep 10
+clear
+
+#Packages
+pkg install curl
+pkg install perl
+pkg install php
+pkg install golang
+pkg install clang
+pkg install python
+pkg install ruby
+pkg install rust
+pkg install bash
+pkg install sl
+pkg install cmatrix
+pkg install nano
+pkg install git
+pkg install libcaca
+pkg install figlet
+pkg install toilet
+pkg install cowsay
+pkg install w3m
+pkg install fish
+pkg install fortune
+pkg install tmate
+pkg install wget
+pkg install fish
+pkg install scrypt
+pkg install termux-api
+pkg install man
+pkg install x11-repo
+pkg install Wireshark-gtk
+pkg install unstable-repo
+pkg install openssh nmap -y
+apt install php wget git httrack -y
+pkg install pigz
+pkg install sox -y
+pkg install irssi -y
+pkg install tor
+pkg install root-repo
+pkg install tsu
+pkg install wireshark-gtk
+pkg install aircrack-ng
+
+#pip modules
+pip install requests
+pip install colorama
+pip install lolcat
+pip install scapy
+pip install requests
+pip install nmap
+pip install impacket
+pip install instaloader
+pip install mechanize
+pip install future
+
+cd $home
+
+#creating folders
+mkdir themes
+mkdir IG
+mkdir FB
+mkdir wifi
+mkdir android
+mkdir tp
+mkdir phishing
+mkdir bombers
+mkdir WB
+mkdir ip
+
+#Cloning Repos
+
+clear
+cd $home
+ls
+sleep 1
+clear
+cd themes
+git clone https://github.com/adi1090x/termux-style
+git clone https://github.com/htr-tech/tstyle
+cd $home
+cd IG
+git clone https://github.com/noob-hackers/ighack
+git clone https://github.com/IncredibleHacker/insta-hack
+git clone https://github.com/Avenge-hacker/Avenge
+git clone https://github.com/noolep/multiBF_ig
+git clone https://github.com/cyberkallan/inshackle-bot.git
+git clone https://github.com/samsesh/insta-bf.git
+git clone https://github.com/th3unkn0n/osi.ig.git
+git clone https://github.com/jxroot/iux
+git clone https://github.com/zxllkada/Get-instagram-users-info---Any-instagram-account
+cd $home
+cd FB
+git clone https://github.com/evait-security/weeman.git
+git clone https://github.com/nasirxo/AK47
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+git clone https://github.com/IAmBlackHacker/Facebook-BruteForce
+git clone https://github.com/fikrado/fikrado.py.git
+git clone https://github.com/Mr-G0b3y/GbyFB
+git clone https://github.com/htr-tech/pakcrack.git
+git clone https://github.com/Ranginang67/Firecrack
+git clone https://github.com/Azim-vau/clone-india
+git clone https://github.com/Oseid/FaceBoom
+git clone https://github.com/dAYOShACKER505/AXOM-FB-HACK
+git clone https://github.com/samsesh/SocialBox-Termux
+git clone https://github.com/NoobHackersCOM/Sensei
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+git clone https://github.com/IlayTamvan/Report
+git clone https://github.com/josifkhan/loggef
+git clone https://github.com/htr-tech/haxorbd.git
+git clone https://github.com/Azim-vau/crack-pro.git
+git clone https://github.com/josifkhan/faceg
+git clone https://github.com/Azim-vau/smbf
+git clone https://github.com/josifkhan/Facebook-Jail
+git clone https://github.com/josifkhan/Widely-Fb-Crack
+git clone https://github.com/josifkhan/Facebook-Jail-India
+cd $home
+cd wifi
+git clone https://github.com/derv82/wifite
+git clone https://github.com/mkdirlove/WI-TOOLKIT
+git clone https://github.com/reverse-shell/routersploit
+git clone https://github.com/esc0rtd3w/wifi-hacker.git
+git clone https://github.com/wifiphisher/wifiphisher.git
+git clone https://github.com/mikinty/Air-Jack
+git clone https://github.com/joswr1ght/cowpatty
+cd $home
+clear
+echo Tools UnderDevelopment.
+echo Installed Successfully
+figlet T-H@ck3r
